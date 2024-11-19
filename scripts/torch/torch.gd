71d3532f73e81997e65af913b524611bae0805df
c7031390 <emanuele.goldin@uibk.ac.at>
2024-11-19 22:43:28 +0100

    Fix dark border for lightpoint

---
+++ scripts/torch/torch.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+# TODO: activate torch when player touches it with an already light up torch
+
+# TODO: define lights and associate it with tile with blown off torches; on activate --> animate torches
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
