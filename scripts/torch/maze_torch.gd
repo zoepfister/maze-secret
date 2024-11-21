b912b7caecbe2c9d7efc55edbdee49c6010d646e
Emanuele.Goldin@student.uibk.ac.at <Emanuele.Goldin@student.uibk.ac.at>
2024-11-21 14:05:24 +0100

    Adapt torch (interactable)

--- scripts/torch/maze_torch.gd
+++ scripts/torch/maze_torch.gd
@@ -1,5 +1,7 @@
 extends AnimatedSprite2D
 
+@onready var player: Player = get_tree().get_first_node_in_group("player")
+
 var animation_name : String = "default"
 var light : PointLight2D
 var trigger : Area2D
@@ -21,6 +23,5 @@ func enable_light() -> void:
 	animation_name = "light_up"
 
 func _on_trigger_area_body_entered(body: Node2D) -> void:
-	if !light.enabled && body.name == "Protagonist":
-		if body.get_has_torch():
-			enable_light()
+	if player.has_torch:
+		enable_light()
