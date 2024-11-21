028ee1ac65b49cb9ff8c5b9835c0e5c8b4bfd1d7
Zoe Pfister <zoe@pfister.pw>
2024-11-21 14:29:01 +0100

    streamline naming

--- scripts/player/EchoLocation/EchoLocation.gd
+++
@@ -1,9 +0,0 @@
-"""
-	Visuals: Concentric circle outgoing from player. Reflected once by walls.
-	When the echolocation hits a wall that is a secret passthrough, the wall shall glow in a blueish color.
-	Optionally, when the echolocation meets the player again, it shall show a compass symbol leading to a secret wall.
-	Audio: Echoing sound when player moves.
-	
-	
-	
-"""
