5787de12860753fc6f0f9fd3750b9baed9562179
Emanuele.Goldin@student.uibk.ac.at <Emanuele.Goldin@student.uibk.ac.at>
2024-11-23 15:33:29 +0100

    Bug fix: first point now is the initial position of the player

--- scenes/thread/thread_script.gd
+++ scenes/thread/thread_script.gd
@@ -1,47 +1,52 @@
-extends Node2D
+extends Line2D
+
+@onready var area_2d : Area2D = $Area2D
 
 @export var tracked : Node2D
-var line : Line2D
-var area_2d : Area2D
-var collision_shape : CollisionShape2D
 
+@export_group("Backtracking settings")
 @export var create_point_treshold : float = 1000.0
 @export var collision_shape_x : float = 2.5
 @export var collision_shape_y : float = 2.5
 @export var offset : float = 0.25
 
 var enabled : bool = false
+var collision_shape : CollisionShape2D
 
 func _ready() -> void:
-	# initialize the line with two point. The start will be fixed; the end will follow the tracked object
-	line = $Line2D
-	area_2d = $Area2D
 	collision_shape = $Area2D/CollisionShape2D
-	line.points = [self.position, tracked.position]
+	# initialize the line with two point. The start will be fixed; the end will follow the tracked object
+	global_position = tracked.global_position
+	points = [tracked.position, tracked.position]
 
 func _process(delta: float) -> void:
 	var tracked_curr_pos = tracked.position
-	line.set_point_position(line.get_point_count() - 1, tracked_curr_pos)
+	var point_count = get_point_count()
+	set_point_position(point_count - 1, tracked_curr_pos)
 	# Draw or erase part of the thread
-	if tracked_curr_pos.distance_to(line.points[line.get_point_count() - 2]) > create_point_treshold:
-		add_point(tracked_curr_pos)
+	if tracked_curr_pos.distance_to(points[point_count - 2]) > create_point_treshold:
+		_add_point(tracked_curr_pos)
 
-func add_point(current_position : Vector2):
-	line.add_point(current_position, line.get_point_count() - 1)
+func _add_point(current_position : Vector2):
+	add_point(current_position, get_point_count() - 1)
 	place_area()
 
 func place_area():
-	if(line.get_point_count() < 3) : return
-	var p1 = line.points[-3]
-	var p2 = line.points[-2]
+	if(get_point_count() < 3) : return
+	var p1 = points[-3]
+	var p2 = points[-2]
 	var direction = p1.direction_to(p2)
 	collision_shape.position = (p2 - p1) * offset + p1
 	collision_shape.rotation = direction.angle()
 	
 func _on_area_2d_body_entered(body:Node2D):
 	if enabled && body == tracked:
-		line.remove_point(line.get_point_count() - 2)
+		_remove_point()
 		place_area()
+
+func _remove_point():
+	if (get_point_count() > 2):
+		remove_point(get_point_count() - 2)
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if !enabled && body == tracked:
