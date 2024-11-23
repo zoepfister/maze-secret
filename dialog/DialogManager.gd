3f52c27a05593a14f39a53306574920c8a2ef8f3
Emanuele.Goldin@student.uibk.ac.at <Emanuele.Goldin@student.uibk.ac.at>
2024-11-23 15:32:25 +0100

    Automatically hide textbox after a certain amount of time

--- dialog/DialogManager.gd
+++ dialog/DialogManager.gd
@@ -1,6 +1,9 @@
 extends Node
 
 @onready var text_box_scene = preload("res://dialog/TextBox.tscn")
+@onready var timer: Timer = $TextBoxTimer
+
+@export var text_box_lifespan: float = 3.0
 
 var dialog_lines: Array[String] = []
 var line_index: int = 0
@@ -26,22 +29,17 @@ func _show_text_box():
 	text_box.finished_displaying.connect(_on_text_box_finished_displaying)
 	get_tree().root.add_child(text_box)
 	text_box.display_text(dialog_lines[line_index])
-	
 	can_advance_line = false
 	
 func _on_text_box_finished_displaying():
 	can_advance_line = true
-	
-func _unhandled_input(event: InputEvent) -> void:
-	if (
-		event.is_action_pressed("interact") &&
-		is_dialog_active &&
-		can_advance_line
-	):
-		text_box.queue_free()
-		line_index += 1
-		if line_index >= dialog_lines.size():
-			is_dialog_active = false
-			line_index = 0
-			return
-		_show_text_box()
+	timer.start(text_box_lifespan)
+
+func _on_text_box_timer_timeout() -> void:
+	text_box.queue_free()
+	line_index += 1
+	if line_index >= dialog_lines.size():
+		is_dialog_active = false
+		line_index = 0
+		return
+	_show_text_box()
