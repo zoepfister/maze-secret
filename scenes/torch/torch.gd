025baa03ab8a83267348942434b49806f87cb9eb
Emanuele.Goldin@student.uibk.ac.at <Emanuele.Goldin@student.uibk.ac.at>
2024-11-23 15:34:07 +0100

    Signal when room is solved

--- scenes/torch/torch.gd
+++ scenes/torch/torch.gd
@@ -3,10 +3,11 @@ extends AnimatedSprite2D
 @onready var interaction_area: InteractionArea = $InteractionArea
 @onready var player: Player = get_tree().get_first_node_in_group("player")
 
+signal room_solved
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_on_interact")
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -14,4 +15,5 @@ func _process(delta: float) -> void:
 	
 func _on_interact():
 	player.has_torch = true
+	room_solved.emit()
 	queue_free()
