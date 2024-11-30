d7a162c49e7fa23e496a3d920bb2c218546cac9a
Emanuele.Goldin@student.uibk.ac.at <Emanuele.Goldin@student.uibk.ac.at>
2024-11-30 18:54:33 +0100

    Load menu after dialog completes

--- scenes/maze/Exit/win_trigger.gd
+++ scenes/maze/Exit/win_trigger.gd
@@ -7,4 +7,12 @@ const win_dialog: Array[String] = [
 ]
 
 func win() -> void:
+	DialogManager.dialog_finished.connect(_on_dialog_finished)
 	DialogManager.start_dialog(player, win_dialog)
+
+func _on_dialog_finished() -> void:
+	get_tree().change_scene_to_file("res://scenes/menus/StartMenu.tscn")
+
+func _on_body_entered(body: Node2D) -> void:
+	win()
+	set_deferred("monitoring", false)	# Deactivate trigger
