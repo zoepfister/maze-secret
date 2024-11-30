ef16ff4374843ccd65aadde4a5f662ed28ed36f7
Emanuele.Goldin@student.uibk.ac.at <Emanuele.Goldin@student.uibk.ac.at>
2024-11-30 18:55:51 +0100

    Refactor room

--- scenes/echo/hide-fake-wall.gd
+++ scenes/echo/hide-fake-wall.gd
@@ -5,3 +5,4 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property(selectedFakeWallLayer, "modulate:a", 0.0, .5)
+	selectedFakeWallLayer.set_process(false)
